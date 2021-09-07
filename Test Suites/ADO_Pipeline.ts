f95c1935233d097a2b14281bad3bc8f6f52ea185
kda0510 <khang.dang@katalon.com>
2021-09-07 15:53:12 +0700

    5596

---
+++ Test Suites/ADO_Pipeline.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ADO_Pipeline</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>75a348c6-b738-4698-ad0c-c9c4cb1f4749</testSuiteGuid>
+   <testCaseLink>
+      <guid>da416bb2-0a25-4f70-871d-26c6c3427f90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reproduce/5596</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
