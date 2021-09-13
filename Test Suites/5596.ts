bc72f2764df907833c99a5ddf4738303d46f5765
khang <khang.dang@katalon.com>
2021-09-13 14:06:20 +0700

    5596

---
+++ Test Suites/5596.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>5596</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b7332cd9-e310-444a-af61-ed15484e9e67</testSuiteGuid>
+   <testCaseLink>
+      <guid>3e326948-6a6c-4421-a3e6-2e36a94917a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/5596</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
