18a171d2514d8edddc63a603b0710dba4893e22a
khang <khang.dang@katalon.com>
2021-09-13 14:55:06 +0700

    ChangeURL_5596

---
+++ Test Suites/ChangeURL_5596.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ChangeURL_5596</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f3d2ddbc-e47c-4527-95aa-5b77aa991c67</testSuiteGuid>
+   <testCaseLink>
+      <guid>9614536e-e949-4942-b4f3-e934731f09d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ChangeURL_5596</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
