5c234b7ace790de6cce7c5e241c3e3b86fad2c6a
khang <khang.dang@katalon.com>
2021-09-13 19:28:15 +0700

    new ts

---
+++ Test Suites/cpqtest.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>cpqtest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b90c7999-5dbd-4931-9900-df3c15d59af5</testSuiteGuid>
+</TestSuiteEntity>
