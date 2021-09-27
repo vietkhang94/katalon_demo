de743a08731bd04f70f3ec05b50bf0fba3a863a3
khang <khang.dang@katalon.com>
2021-09-27 15:46:58 +0700

    next commit

--- Test Suites/Email_Demo.ts
+++ Test Suites/Email_Demo.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>16a4eb19-d7f5-4ec1-8a20-24413f3e86eb</testSuiteGuid>
+   <testCaseLink>
+      <guid>d4aba514-18f1-4044-bb14-ec9578bbf909</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/cpqtest</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/cpqtest.ts
+++ Test Suites/cpqtest.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>b90c7999-5dbd-4931-9900-df3c15d59af5</testSuiteGuid>
+   <testCaseLink>
+      <guid>0b04854a-1ac5-4245-933c-260a34a2b548</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/cpqtest</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
